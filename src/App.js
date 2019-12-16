9d76e7f7da3eb4da948b055e119d8b522e272f7c
Ivan Stelmakh <stelmakh.developer@gmail.com>
2019-12-16 20:58:38 +0200

    Added react-icons with basic structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import Header from './components/layout/Header'
+import Content from './components/layout/Content'
 
-const App = () => <Header />
+const App = () => (
+  <div className="App">
+    <Header />
+    <Content />
+  </div>
+)
 
 export default App
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { FaPizzaSlice } from 'react-icons/fa'
 
 const Header = () => {
   return (
     <header className="header" data-testid="header">
-      Header
+      <nav>
+        <div className="logo">
+          <img src="/images/logo.png" alt="Todoist" />
+        </div>
+        <div className="settings">
+          <ul>
+            <li>+</li>
+            <li>
+              <FaPizzaSlice />
+            </li>
+          </ul>
+        </div>
+      </nav>
     </header>
   )
 }
