052ea93eacf7873afd34e8a348703e79cbae05bd
Ivan Stelmakh <stelmakh.developer@gmail.com>
2020-01-05 16:45:24 +0200

    Checkbox action

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -2,19 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import firebase from '../firebase'
 
-const Checkbox = ({ id }) => {
+const Checkbox = ({ id, taskDesc }) => {
   const archiveTask = () => {
-    firebase.firestore
+    firebase
+      .firestore()
       .collection('tasks')
       .doc(id)
-      .update({ archived: true })
+      .update({
+        archived: true
+      })
   }
   return (
     <div
       className="checkbox-holder"
       data-testid="checkbox-action"
-      onClick={() => {}}
+      onClick={archiveTask}
       onKeyPress={archiveTask}
+      aria-label={`Mark ${taskDesc} as done?`}
       role="button"
       tabIndex="0"
     >
@@ -24,7 +28,8 @@ const Checkbox = ({ id }) => {
 }
 
 Checkbox.propTypes = {
-  id: PropTypes.string.isRequired
+  id: PropTypes.string.isRequired,
+  taskDesc: PropTypes.string.isRequired
 }
 
 export default Checkbox
